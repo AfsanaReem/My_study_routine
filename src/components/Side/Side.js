3974db455d6ff67f0eaf29c6f2fc95b4853f5576
Reem <apple@Apples-MacBook-Pro.local>
2022-09-29 02:23:36 +0600

    fakeData created

--- src/components/Side/Side.js
+++ src/components/Side/Side.js
@@ -8,6 +8,11 @@ const Side = () => {
                 <img src='favicon.ico'></img>
                 <h4>Afsana Rahman <small>Computer Engineer</small></h4>
             </div>
+            <div className='credit-info'>
+                <h4>136 <small>Credit</small></h4>
+                <h4>3.06 <small>CGPA</small></h4>
+                <h4>4 <small>Years</small></h4>
+            </div>
 
         </div>
     );
