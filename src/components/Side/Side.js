3df7d410c216e961ef76f7be1e0e69137f34fe00
Reem <apple@Apples-MacBook-Pro.local>
2022-09-29 01:03:25 +0600

    Components created

---
+++ src/components/Side/Side.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Side.css'
+
+const Side = () => {
+    return (
+        <div className='side-component'>
+            <div className='my-info'>
+                <img src='favicon.ico'></img>
+                <h4>Afsana Rahman <small>Computer Engineer</small></h4>
+            </div>
+
+        </div>
+    );
+};
+
+export default Side;
